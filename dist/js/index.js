6c7ea4b13dddcdf6deddfc946db59029c1818b6e
Solo Traze <traze.solo.dev@gmail.com>
2016-07-19 00:45:47 +0530

    locationHelper basic

--- dist/js/index.js
+++ dist/js/index.js
@@ -1,6 +1,7 @@
 $(document).ready(function() {
   $('#btnGetLocation').click(function() {
-    var location = 'co-ords';
+    var lc = new locationHelper();
+    var location = lc.getLocation();
     $('#dvContent').text(location);
   });
 });
---
+++ dist/js/locationHelper.js
@@ -0,0 +1,6 @@
+var locationHelper = function() {
+};
+
+locationHelper.prototype.getLocation = function() {
+  return '22,55';
+};
